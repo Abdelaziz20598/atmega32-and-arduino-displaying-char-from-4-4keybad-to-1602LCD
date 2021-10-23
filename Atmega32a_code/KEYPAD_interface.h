3a800751926047b6ba0df848d9394fd8322ff191
Bassem Hussein <75940885+bassemhussein-code@users.noreply.github.com>
2021-10-24 01:49:38 +0200

    Add files via upload

---
+++ Atmega32a_code/KEYPAD_interface.h
@@ -0,0 +1,28 @@
+/*****************************************************************/
+/* Autor        : BASSEM HUSSEIN                                 */ 
+/* Date         : 24/10/2021                                     */ 
+/* File         : KEYPAD_INTERFACE                               */ 
+/* Version      : V1.0                                           */ 
+/* LOG          :                                                */ 
+/*****************************************************************/ 
+
+
+#ifndef  KEYPAD_INTERFACE_H 
+#define  KEYPAD_INTERFACE_H 
+
+
+#define  GPIOA    0 
+#define  GPIOB    1 
+#define  GPIOC    2 
+#define  GPIOD    3
+
+
+
+u8 keypad_u8checkPress(u8 copy_u8Port) ; 
+
+
+
+
+
+
+#endif 
